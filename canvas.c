9dc62fc4172881ac2ece84fcb0aeec78b627b88c
Osteofelidae <115187283+osteofelidae@users.noreply.github.com>
2023-11-15 14:04:32 -0700

    AccountCalendar struct and functions

--- canvas.c
+++ canvas.c
@@ -167,6 +167,52 @@ int doreq(  // Send a request
 
 // PUBLICS ========================================================================================
 
+// STUCT FUNCTIONS --------------------------------------------------------------------------------
+AccountCalendar *malloc_AccountCalendar(  // Malloc AccountCalendar struct
+	char *id,  // The ID of the account associated with this calendar
+	char *name,  // The name of the account associated with this calendar
+	char *parent_account_id,  // The account's parent ID, or null if this is the root account
+	char *root_account_id,  // The ID of the root account, or null if this is the root account
+	int visible,  // Whether this calendar is visible to users
+	int auto_subscribe,  // Whether users see this calendar's events without needing to manually add it
+	int sub_account_count,  // Number of this account's direct sub-accounts
+	char *asset_string,  // Asset string of the account
+	char *type,  // Object type
+	char *calendar_event_url,  // URL to get full detailed events
+	int can_create_calendar_events,  // Whether the user can create calendar events
+	char *create_calendar_event_url,  // API path to create events for the account
+	char *new_calendar_event_url  // URL to open the more options event editor
+	) {
+
+	AccountCalendar *obj = malloc(sizeof(AccountCalendar));  // Allocate memory
+
+	obj->id = id;  // Set members
+	obj->name = name;
+	obj->parent_account_id = parent_account_id;
+	obj->root_account_id = root_account_id;
+	obj->visible = visible;
+	obj->auto_subscribe = auto_subscribe;
+	obj->sub_account_count = sub_account_count;
+	obj->asset_string = asset_string;
+	obj->type = type;
+	obj->calendar_event_url = calendar_event_url;
+	obj->can_create_calendar_events = can_create_calendar_events;
+	obj->create_calendar_event_url = create_calendar_event_url;
+	obj->new_calendar_event_url = new_calendar_event_url;
+
+	return obj;
+
+}
+int free_AccountCalendar(  // Free AccountCalendar struct
+	AccountCalendar* obj  // AccountCalendar struct to free
+	) {
+
+	free(obj);  // Free struct
+
+	return 0;
+
+}
+
 // FUNCTIONS --------------------------------------------------------------------------------------
 int init() {
 
--- canvas.h
+++ canvas.h
@@ -4,3 +4,40 @@
 int init();
 
 int clean();
+
+
+// STRUCTS ----------------------------------------------------------------------------------------
+typedef struct AccountCalendar {
+	char *id;  // The ID of the account associated with this calendar
+	char *name;  // The name of the account associated with this calendar
+	char *parent_account_id;  // The account's parent ID, or null if this is the root account
+	char *root_account_id;  // The ID of the root account, or null if this is the root account
+	int visible;  // Whether this calendar is visible to users
+	int auto_subscribe;  // Whether users see this calendar's events without needing to manually add it
+	int sub_account_count;  // Number of this account's direct sub-accounts
+	char *asset_string;  // Asset string of the account
+	char *type;  // Object type
+	char *calendar_event_url;  // URL to get full detailed events
+	int can_create_calendar_events;  // Whether the user can create calendar events
+	char *create_calendar_event_url;  // API path to create events for the account
+	char *new_calendar_event_url;  // URL to open the more options event editor
+
+} AccountCalendar;
+AccountCalendar *malloc_AccountCalendar(  // Malloc AccountCalendar struct
+	char *id,  // The ID of the account associated with this calendar
+	char *name,  // The name of the account associated with this calendar
+	char *parent_account_id,  // The account's parent ID, or null if this is the root account
+	char *root_account_id,  // The ID of the root account, or null if this is the root account
+	int visible,  // Whether this calendar is visible to users
+	int auto_subscribe,  // Whether users see this calendar's events without needing to manually add it
+	int sub_account_count,  // Number of this account's direct sub-accounts
+	char *asset_string,  // Asset string of the account
+	char *type,  // Object type
+	char *calendar_event_url,  // URL to get full detailed events
+	int can_create_calendar_events,  // Whether the user can create calendar events
+	char *create_calendar_event_url,  // API path to create events for the account
+	char *new_calendar_event_url  // URL to open the more options event editor
+	);
+int free_AccountCalendar(  // Free AccountCalendar struct
+	AccountCalendar* obj  // AccountCalendar struct to free
+	);
